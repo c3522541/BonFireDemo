bf91669b66126342b5f9ec33b484fdf609e31507
Sean Che <seangogo@gmail.com>
2011-06-09 23:15:32 -0400

    Initial Commit

---
+++ CampFire/CampFireAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  CampFireAppDelegate.h
+//  CampFire
+//
+//  Created by Sean Che on 11-06-09.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CampFireAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
