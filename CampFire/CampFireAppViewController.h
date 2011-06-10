d12b934e45c3c3d56a63e3ba191605a6ea18a5a0
Sean Che <seangogo@gmail.com>
2011-06-09 23:23:19 -0400

    add CampFireAppViewController

---
+++ CampFire/CampFireAppViewController.h
@@ -0,0 +1,16 @@
+//
+//  CampFireAppViewController.h
+//  CampFire
+//
+//  Created by Sean Che on 11-06-09.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CampFireAppViewController : UIViewController {
+    
+}
+
+@end
