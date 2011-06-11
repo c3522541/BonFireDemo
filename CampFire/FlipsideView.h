4b96790732cb16a653659533e21efad3ea8b8df0
Sean Che <seangogo@gmail.com>
2011-06-11 00:21:31 -0400

    fixing

---
+++ CampFire/FlipsideView.h
@@ -0,0 +1,16 @@
+//
+//  FlipsideView.h
+//  CampFire
+//
+//  Created by Sean Che on 11-06-10.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FlipsideView : UIViewController {
+    
+}
+
+@end
