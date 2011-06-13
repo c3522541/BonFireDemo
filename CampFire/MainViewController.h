3f0e65921033c3e344c26bb279df94c50eb26a4c
c3522541 <seangogo@gmail.com>
2011-06-13 14:09:59 -0400

    add MainViewController

---
+++ CampFire/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  CampFire
+//
+//  Created by Sean Che on 11-06-13.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainViewController : UIViewController {
+    
+}
+
+@end
